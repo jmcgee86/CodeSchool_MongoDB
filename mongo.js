1c2967b377231f63abbf40f6916beaa10de8106a
Jim McGee <jmcgee86@gmail.com>
2018-01-27 17:37:14 +0000

    level one started, need to check

---
+++ mongo.js
@@ -0,0 +1,47 @@
+// create database
+// use myGame
+
+//create collection called monsters and add first document
+    // db.monsters.insert(
+    //     {
+    //         "name": "Frank",
+    //         "health": "good",
+    //         "lastFought": new Date (2018, 0, 27),
+    //         "attacks": ["bite", "scratch", "kick"],
+    //         "stats": {"attack": 5, "defense": 10}
+    //     })
+
+//add second document to monsters collection
+    // db.monsters.insert(
+    //     {
+    //         "name": "Demogorgan",
+    //         "health": "ppor",
+    //         "lastFought": new Date (2017, 11, 20),
+    //         "attacks": ["inhabit", "throw", "choke"],
+    //         "stats": {"attack": 10, "defense": 2}
+    //     })
+
+//find monster based on a name 
+db.monsters.find({"name": "Frank"})
+
+//find monster based on one of the attacks
+db.monsters.find({"attacks": "bite"})
+
+//find monster based on defense stat
+db.monsters.find({"stats.defense": 2})
+
+var db = './mongod';
+
+function addMonster(name, health, lastFought, attacks, stats){
+    db.monsters.insert({
+        name: "",
+        health: "",
+        lastFought: "",
+        attacks: [],
+        stats:{"attack":"",
+            "defense":""    
+        }
+    })
+}
+
+console.log(db.monsters.find());

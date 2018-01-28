fdd4112d4a5bdff0ca77d61bd4762b8a3972cbfe
Jim McGee <jmcgee86@gmail.com>
2018-01-28 13:06:53 +0000

    completed level three

--- mongo.js
+++ mongo.js
@@ -45,3 +45,28 @@ function addMonster(name, health, lastFought, attacks, stats, db){
 }
 
 console.log(db.monsters.find());
+
+//level2
+
+var mongo = function(db){
+//Write the command that will find all monsters with an attack lower than 10
+    db.monsters.find({"stats.attack": {"$lt": 10}})
+//Write the command that will find all monsters with a level above 5 but below 15 inclusive
+    db.monsters.find({"level": {"$gt": 5, "$lte": 15}})
+//Write the command to find monsters that do not have the “bite” attack.
+    db.monsters.find({"attacks": {"$ne": "bite"}})
+//Write the command to find monsters with levels less than 6 but only return the name, level and health attributes
+    db.monsters.find(
+        {"level": {"$lt": 6}},
+        {"name": true, "level": true, "health": true}
+        )
+//Write the command to find all monsters who attack is between 10 and 20 but do not include the monsters health or style
+    db.monster.find(
+        {"stats.attack": {"$gt":10, "$lt": 20 }},
+        {"health": false, "style": false}
+        )
+//Write the command to find out how many monsters are in the collection.
+    db.monsters.find().count()
+//Write the command to sort the collection by the monsters level with the highest level at the top and the lowest at the bottom
+    db.monsters.find().sort({"level": -1})
+};
